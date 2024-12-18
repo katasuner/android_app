b52429b9d0f64e0cb92c89f9a190bbf8a079915c
katasuner <rupasova19955@gmail.com>
2024-12-19 00:28:47 +0400

    Version 1.0.1 Event DESC

--- app/src/main/java/com/example/mooddiary/EventDao.kt
+++ app/src/main/java/com/example/mooddiary/EventDao.kt
@@ -4,12 +4,13 @@ import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.Query
 import androidx.room.Delete
+import androidx.room.Update
 import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface EventDao {
     // Получение всех событий
-    @Query("SELECT * FROM events")
+    @Query("SELECT * FROM events ORDER BY id DESC")
     fun getAllEvents(): Flow<List<Event>> // Flow для реактивного получения данных
 
     // Вставка события
@@ -23,4 +24,7 @@ interface EventDao {
     // Получение события по ID
     @Query("SELECT * FROM events WHERE id = :eventId LIMIT 1")
     suspend fun getEventById(eventId: Int): Event
+
+    @Update
+    suspend fun updateEvent(event: Event)
 }
